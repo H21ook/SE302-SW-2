3f42e210bf938344393852377abe1101d7b085a5
Hishigbayr <hishigbayr_3@yahoo.com>
2017-04-19 12:09:51 +0800

    Revert "Revert "CarRacing""
    This reverts commit f14a8b1d30cd9ee8a7c709005aa6dca784e89256.

---
+++ ConsoleApplication2/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Car
+    {
+        public int X { get; set; } // x coordinat
+        public int Y { get; set; } // y coordinat
+        public static int LENGTH = 3; // mashinii urt
+        public static int WIDTH = 4;// mashinii orgon
+
+        public string[,] carbody = new string[,]
+        {
+            {"0"," "," ","0"},
+            {" ","(",")"," "},
+            {"0"," "," ","0"}
+        };
+        public string[,] carDestroyBody = new string[,]
+        {
+            {"X"," "," ","X"},
+            {" ","X","X"," "},
+            {"X"," "," ","X"}
+        };
+        public Car(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Move(int x, int y)
+        {
+            X += x;
+            DrawDisplay d = new DrawDisplay();
+            d.DrawCar(this);
+
+            for (int i = 0; i < LENGTH; i++)
+            {
+                if (x == -1)
+                    Console.SetCursorPosition(X + WIDTH, Y + i);
+                else
+                    Console.SetCursorPosition(X - 1, Y + i);
+                Console.Write(" ");
+            }
+        }
+    }
+}
