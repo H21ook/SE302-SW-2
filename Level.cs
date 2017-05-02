d1159cc902a356c53229b973b02e22bb09dbfaf9
PuujeeRo <purewsurenbatsuuri1234@gmail.com>
2017-05-02 11:50:48 +0900

    Add files via upload
    Программын score нь level.cs -н  lvladdscore()-той тэнцүү болох үед timerspeed-г 100 гаар багасгаж тоглоомны хурд нэмэгдэнэ тэгээд levelnumber- г нэгээр нэмэгдүүлж саадыг уртасгана.

---
+++ Level.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Level
+    {
+        public int levelnumber = 1;
+        public int lvladdscore = 2000;
+        public int maxlevel = 3;
+
+        public void levelUp()
+        {
+            if (Program.score == lvladdscore && levelnumber <= maxlevel)
+            {
+                levelnumber += 1;
+                Program.timerSpeed -= 100;
+                lvladdscore += lvladdscore;
+            }
+        }
+
+        public Problem levelOne(int newXCoordinat)
+        {
+            Problem problem = new Problem(newXCoordinat, 0, new string[] { "#", "#", "#" });
+            return problem;
+        }
+        public Problem levelTwo(int newXCoordinat)
+        {
+            Problem problem = new Problem(newXCoordinat, 0, new string[] { "#", "#", "#", "#" });
+            return problem;
+        }
+        public Problem levelThree(int newXCoordinat)
+        {
+            Problem problem = new Problem(newXCoordinat, 0, new string[] { "#", "#", "#", "#", "#" });
+            return problem;
+        }
+    }
+}
