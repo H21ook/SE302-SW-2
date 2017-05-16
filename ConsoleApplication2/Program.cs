d546c193ad5a1baa0cfa5bd605a31de9b74561a0
Hishigbayr <H21ook@users.noreply.github.com>
2017-05-16 10:54:25 +0900

    Update Program.cs
    MoveGame() function-g jijigsgesen

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,125 +8,76 @@ namespace ConsoleApplication2
 {
     public class Program
     {
-        private static int screenWidth = 18;
-        private static int screenHeight = 30;
-        private static int livesCount = 3;
-        private static int score = 0;
-        private static int timerSpeed = 300;
-        private static int speed = 0;
-        private static List<Problem> problems = new List<Problem>();
+        public const int SCREENWIDTH = 18;
+        public const int SCREENHEIGTH = 30;
+        private const int MAXSPEED = 400;
+
+        public static int livesCount = 3;
+        public static int score = 0;
+        public static int timerSpeed = 300;
+        public static int speed = 0;
+        public static bool suirel = false;
+        public static List<Problem> problems = new List<Problem>();
+        
         public static void Main(string[] args)
         {
             Console.BufferHeight = Console.WindowHeight = 31;
             Console.BufferWidth = Console.WindowWidth = 50;
             Console.CursorVisible = false;
+            int start = 0;
+            start = Menu();
+            
+            if (start == 1)
+            {
+                
+                Car car = new Car((SCREENWIDTH / 2) - Car.WIDTH / 2, SCREENHEIGTH - Car.LENGTH);
+                DrawDisplay d = new DrawDisplay();
 
-            DrawDisplay d = new DrawDisplay();
-            d.DrawRoad(screenHeight, screenWidth);
+                d.ClearConsole(Console.BufferHeight - 1, Console.BufferWidth - 1);
 
-            Car car = new Car((screenWidth/2) - Car.WIDTH/2 ,screenHeight- Car.LENGTH);
-            d.DrawCar(car);
-
-            MoveGame(car);
+                d.DrawRoad(SCREENHEIGTH, SCREENWIDTH);
+                d.DrawCar(car);
+            
+                Program.MoveGame(car);
+            }
         }
         public static void MoveGame(Car car)
         {
             DrawDisplay d = new DrawDisplay();
             Random rand = new Random();
-            int newXCoordinat = 0;
-            int q = 0;
+            Level l = new Level();
+            l.levelnumber = 1;
+            int proSpace = 0;
+
             while (true)
             {
-                int lvladd = 2000;
-                int lvl = 1;
-                if (score == lvladd && lvl< 4)
-                {
-                    lvl += 1;
-                    timerSpeed -= 100;
-                    lvladd += 2000;
-                }
+                l.levelUp();
                 score += 10;
                 speed += 20;
-                if (speed > 400)
+
+                if (speed > MAXSPEED)
                 {
-                    speed = 400;
+                    speed = MAXSPEED;
                 }
-                bool suirel = false;
-                {
-                    if (q % 4 == 0)
-                    {
-                        newXCoordinat = rand.Next(1, screenWidth - 4);
-                        Level l = new Level();
-                        Problem proObj;
-                        switch(lvl)
-                        {
-                            case 1:
-                                proObj = l.levelOne(newXCoordinat);
-                                problems.Add(proObj);
-                                break;
-                            case 2:
-                                proObj = l.levelTwo(newXCoordinat);
-                                problems.Add(proObj);
-                                break;
-                            case 3:
-                                proObj = l.levelThree(newXCoordinat);
-                                problems.Add(proObj);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-                q++;
+                suirel = false;
+
+                Problem.CreateProblem(proSpace,l);
+
+                proSpace++;
+
                 while (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-                    switch (pressedKey.Key)
-                    {
-                        case ConsoleKey.RightArrow:
-                            if (car.X < (screenWidth - 1) - Car.WIDTH)
-                            {
-                                car.Move(1, 0);
-                            }
-                            break;
+                    car.Rotate(pressedKey);
+                }
 
-                        case ConsoleKey.LeftArrow:
-                            if (car.X > screenWidth - (screenWidth - 1))
-                            {
-                                car.Move(-1, 0);
-                            }
-                            break;
-                    }
-                }
                 List<Problem> newList = new List<Problem>();
-                for (int i = 0; i < problems.Count; i++)
-                {
-                    Problem oldProblem = problems[i];
-                    Problem newproObj = new Problem(oldProblem.coordinatX, oldProblem.coordinatY + 1, oldProblem.color, oldProblem.problem);
-                    for (int j = 0; j < newproObj.problem.Length; j++)
-                    {
-                        if ((newproObj.coordinatY == car.Y || newproObj.coordinatY == car.Y + 1 || newproObj.coordinatY == car.Y + 2) && (newproObj.coordinatX == car.X || newproObj.coordinatX == car.X + 1 || newproObj.coordinatX == car.X + 2 || newproObj.coordinatX == car.X + 3 || newproObj.coordinatX + 1 == car.X || newproObj.coordinatX + 1 == car.X + 1 || newproObj.coordinatX + 1 == car.X + 2 || newproObj.coordinatX + 1 == car.X + 3 || newproObj.coordinatX + 2 == car.X || newproObj.coordinatX + 2 == car.X + 1 || newproObj.coordinatX + 2 == car.X + 2 || newproObj.coordinatX + 2 == car.X + 3 || newproObj.coordinatX + 3 == car.X || newproObj.coordinatX + 3 == car.X + 1 || newproObj.coordinatX + 3 == car.X + 2 || newproObj.coordinatX + 3 == car.X + 3))
-                        {
-                            suirel = true;
-                            speed = 0;
-                            if (livesCount <= 0)
-                            {
-                                
-                                d.DrawInformation(screenWidth+7, screenHeight/2, "GAME OVER", ConsoleColor.Red);
-                                d.DrawInformation(screenWidth+5, (screenHeight/2)+1, "Press [enter] to exit", ConsoleColor.Red);
-                                d.DestroyDrawCar(car);
-                                Console.ReadLine();
-                                Environment.Exit(0);
-                            }
-                        }
-                    }
-                    if (newproObj.coordinatY < Console.WindowHeight)
-                    {
-                        newList.Add(newproObj);
-                    }
-                }
+                newList = Problem.MoveProblem(car);
+
                 problems = newList;
-                d.ClearConsole(screenHeight,screenWidth);
+
+                d.ClearConsole(SCREENHEIGTH, SCREENWIDTH);
+
                 if (suirel)
                 {
                     livesCount--;
@@ -137,14 +88,31 @@ namespace ConsoleApplication2
                 {
                     d.DrawCar(car);
                 }
+
                 foreach (Problem pro in problems)
                 {
                     d.DrawProblem(pro);
                 }
-                d.DrawInformation(screenWidth + 5, screenHeight / 3, "Lives: " + livesCount, ConsoleColor.White);
-                d.DrawInformation(screenWidth + 5, screenHeight / 3 + 1, "Speed: " + speed, ConsoleColor.White);
-                d.DrawInformation(screenWidth + 5, screenHeight / 3 + 2, "Score: " + score, ConsoleColor.White);
+
+                d.DrawInformation(SCREENWIDTH + 5, SCREENHEIGTH / 3, "Lives: " + livesCount, ConsoleColor.White);
+                d.DrawInformation(SCREENWIDTH + 5, SCREENHEIGTH / 3 + 1, "Speed: " + speed, ConsoleColor.White);
+                d.DrawInformation(SCREENWIDTH + 5, SCREENHEIGTH / 3 + 2, "Score: " + score, ConsoleColor.White);
+
                 System.Threading.Thread.Sleep(timerSpeed);
+            }
+        }
+
+        public static void isEnd(Car car)
+        {
+            DrawDisplay d = new DrawDisplay();
+
+            if (livesCount <= 0)
+            {
+                d.DrawInformation(Program.SCREENWIDTH + 7, Program.SCREENHEIGTH / 2, "GAME OVER", ConsoleColor.Red);
+                d.DrawInformation(Program.SCREENWIDTH + 5, (Program.SCREENHEIGTH / 2) + 1, "Press [enter] to exit", ConsoleColor.Red);
+                d.DestroyDrawCar(car);
+                Console.ReadLine();
+                Environment.Exit(0);
             }
         }
     }
