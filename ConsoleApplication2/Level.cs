92c3e918cab55845733a3d2e435ee05570073389
PuujeeRo <purewsurenbatsuuri1234@gmail.com>
2017-05-16 11:04:48 +0900

    Update Level.cs
    level zoohon oorchlow

--- ConsoleApplication2/Level.cs
+++ ConsoleApplication2/Level.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +8,33 @@ namespace ConsoleApplication2
 {
     public class Level
     {
-        public int levelnumber {get;set;}
-        public Problem problem;
+        public int levelnumber = 1;
+        public int lvladdscore = 2000;
+        public int maxlevel = 3;
+
+        public void levelUp()
+        {
+            if (Program.score == lvladdscore && levelnumber <= maxlevel)
+            {
+                levelnumber += 1;
+                Program.timerSpeed -= 100;
+                lvladdscore += lvladdscore;
+            }
+        }
+
         public Problem levelOne(int newXCoordinat)
         {
-            problem = new Problem(newXCoordinat, 0, ConsoleColor.Blue, new string[] { "#", "#", "#", "#" });
+            Problem problem = new Problem(newXCoordinat, 0, new string[] { "#", "#", "#" });
             return problem;
         }
         public Problem levelTwo(int newXCoordinat)
         {
-            problem = new Problem(newXCoordinat, 0, ConsoleColor.Blue, new string[] { "#", "#", "#", "#"});
+            Problem problem = new Problem(newXCoordinat, 0, new string[] { "#", "#", "#", "#" });
             return problem;
         }
         public Problem levelThree(int newXCoordinat)
         {
-            problem = new Problem(newXCoordinat, 0, ConsoleColor.Blue, new string[] { "#", "#", "#", "#"});
+            Problem problem = new Problem(newXCoordinat, 0, new string[] { "#", "#", "#", "#", "#" });
             return problem;
         }
     }
